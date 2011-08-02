fd2d8b3fcd8652d7f729a98bb0c7238713dee17a
Nguyen Thi Thanh Thuy <thanhthuynguyenhd@gmail.com>
2011-08-02 11:20:52 +0700

    Test 02081011

--- components/com_documentlibrary/views/homepage/tmpl/default.php
+++ components/com_documentlibrary/views/homepage/tmpl/default.php
@@ -11,9 +11,41 @@
 //$input->render();
 
 ?>
-<table>
+<table width='100%'>
 	<tr>
-		<td></td>ccccccccccccccccccc
-		<td></td>
+		<td colspan='2' width='70%'>
+			Giới thiệu về trang web<br>
+			Hướng dẫn sử dụng<br>
+			Giới thiệu hệ thống tính điểm
+		</td>
+		<td>
+			Đóng góp tài liệu
+		</td>
+	</tr>
+	<tr>
+		<td colspan='2'>
+			Thông tin giáo dục<br>
+		</td>
+		<td>
+			Tìm kiếm:<input type='text'><br>
+			Tìm kiếm nâng cao:<input type='text'><br>
+			Tìm kiếm theo số tài liệu:
+		</td>
+	</tr>
+	<tr>
+		<td>
+			10 thành viên có số điểm cao nhất
+		</td>
+		<td>
+			10 tài liệu đóng góp mới nhất
+		</td>
+		<td>
+			Thống kê:<br>
+			Ngày/ Tuần/Tháng/Năm<br>
+			Số tài liệu<br>
+			Số thành viên<br>
+			Số lượt đăng nhập<br>
+			Số người online<br>
+		</td>
 	</tr>
 </table>
