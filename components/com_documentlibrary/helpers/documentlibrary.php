6a1a0b19fa5b02422109b3c06ea97a62c9bde9f0
Nguyen Thanh Trung <nguyenthanh.trung@nomovok.com>
2011-08-03 22:52:39 +0700

    added some more functions to helper, improved uiText() function

--- components/com_documentlibrary/helpers/documentlibrary.php
+++ components/com_documentlibrary/helpers/documentlibrary.php
@@ -46,13 +46,54 @@ abstract class DocumentLibraryHelper {
 		DocumentLibraryHelper::$uiTextPrefix = $prefix;
 	}
 	
-	public static function uiText($text) {
-		$prefix = DocumentLibraryHelper::$uiTextPrefix;
-		if (empty ($prefix)) {
-			$prefix = '';
+	// public static function uiText($text) {
+		// $prefix = DocumentLibraryHelper::$uiTextPrefix;
+		// if (empty ($prefix)) {
+			// $prefix = '';
+		// }
+// 		
+		// return JText::_($prefix . $text);
+	// }
+	
+	// more customizable version
+	// example uiText('document_title', 'view', 'document') => COM_DOCUMENT_LIBRARY_VIEW_DOCUMENT_DOCUMENT_TITLE
+	// uiText('document_title', 'generic') => COM_DOCUMENT_LIBRARY_GENERIC_DOCUMENT_TITLE
+	public static function uiText($text, $type = '', $class = '') {
+		static $prefix = 'COM_DOCUMENT_LIBRARY_';
+		if (!empty($type)) {
+			$type = strtoupper($type) . '_';
+		}
+		if (!empty($class)) {
+			$class = strtoupper($class) . '_';
 		}
 		
-		return JText::_($prefix . $text);
+		if (!empty($type) || !empty($class)) {
+			return JText::_($prefix . $type . $class . $text);
+		}
+		
+		// else old version
+		return JText::_(DocumentLibraryHelper::$uiTextPrefix . $text);
+	}
+	
+	public static function documentNumber($original_id, $version, $document_id) {
+		// NOTE: future of this one may be some db query
+		if ($original_id == 0) {
+			$original_id = $document_id;
+		}
+		return $original_id . '.' . $version;
+	}
+	
+	/**
+	 * used to check viewAll request for comments + downloads
+	 */
+	public static function viewAllValue() {
+		$viewAll = JRequest::getVar('viewAll', false);
+		if ($viewAll == 'false') {
+			$viewAll = false;
+		} else if ($viewAll == 'true') {
+			$viewAll = true;
+		}
+		return $viewAll;
 	}
 }
 ?>
