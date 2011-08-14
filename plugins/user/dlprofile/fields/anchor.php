2c7f10f81ab9206811f16983f78b673960c0afd5
Nguyen Thanh Trung <nguyenthanh.trung@nomovok.com>
2011-08-14 11:33:46 +0700

    user dl profile plugin

---
+++ plugins/user/dlprofile/fields/anchor.php
@@ -0,0 +1,37 @@
+<?php
+defined ('_JEXEC') or die;
+
+// jimport ('joomla.form.formfield');
+
+// class JFormFieldAnchor {
+	// protected $type="anchor";
+// 	
+	// public function getLabel() {
+		// $query = array();
+		// if (isset($this->component)) {
+			// $query[] = 'option=com_' . $this->component;
+		// }
+		// if (isset($this->view)) {
+			// $query[] = 'view=' . $this->view;
+		// }
+// 		
+		// if (isset($this->param)) {
+// 			
+		// }
+		// $href = '';
+		// if (isset($this->query) && !empty($this->query)) {
+			// $href = JRoute::_('index.php?' . $query);
+		// } else {
+			// $href = JRequest::getURI();
+		// }
+// 		
+		// var_dump('shit !');
+// 		
+		// return '<a href="http://www.google.com">' . $this->label . '</a>';
+	// }
+// 	
+	// public function getInput() {
+		// return '<a href="http://www.google.com">' . $this->label . '</a>';
+	// }
+// }
+?>
