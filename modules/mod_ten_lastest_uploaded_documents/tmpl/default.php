1cf52cf27b517f60e41ccba8b394782b925703a1
Nguyen Thi Thanh Thuy <thanhthuynguyenhd@gmail.com>
2011-08-15 16:50:07 +0700

    Update

--- modules/mod_ten_lastest_uploaded_documents/tmpl/default.php
+++ modules/mod_ten_lastest_uploaded_documents/tmpl/default.php
@@ -10,7 +10,7 @@ defined ('_JEXEC') or die ('Access denied');
 						<a href='<?php echo JRoute::_('index.php?option=com_documentlibrary&task=document&document=' . $doc->document_id);?>'>
 						<?php echo $doc->title; ?></a>
 						&nbsp;<i>(v<?php echo $doc->version?>)</i>
-						&nbsp;uploaded by <b><?php echo $doc->uploader?>&nbsp;</b><i>(<?php echo $doc->uploaded_date; ?>)</i>
+						&nbsp;</b><i>(<?php echo $doc->uploaded_date; ?>)</i>
 					</li>
 				<?php endforeach;?>
 			</ol>
