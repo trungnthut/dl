746fed78b2315b8ea1e738effe52706324b8a05c
Nguyen Thi Thanh Thuy <thanhthuynguyenhd@gmail.com>
2011-08-25 16:05:37 +0700

    Update

--- modules/mod_ten_most_interested_documents/tmpl/default.php
+++ modules/mod_ten_most_interested_documents/tmpl/default.php
@@ -9,7 +9,7 @@ defined ('_JEXEC') or die ('Access denied');
 					<li>
 						<a href='<?php echo JRoute::_('index.php?option=com_documentlibrary&task=document&document=' . $doc->document_id);?>'>						<?php echo $doc->title; ?></a>
 						&nbsp;<i>(v<?php echo $doc->version?>)</i>
-						&nbsp;<b><?php echo $doc->total_comment?>&nbsp;</b><i>comments</i>
+						&nbsp;<b><?php echo $doc->total_comment?>&nbsp;</b><i><?php echo JText::_("MOD_TEN_MOST_INTERESTED_DOCUMENTS_COMMENT")?></i>
 					</li>
 				<?php endforeach;?>
 			</ol>
