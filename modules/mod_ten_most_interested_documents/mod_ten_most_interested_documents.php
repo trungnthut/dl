b545d6532890e2defcf2d289f592775b89281dc6
Nguyen Thi Thanh Thuy <thanhthuynguyenhd@gmail.com>
2011-08-25 16:04:59 +0700

    Update

--- modules/mod_ten_most_interested_documents/mod_ten_most_interested_documents.php
+++ modules/mod_ten_most_interested_documents/mod_ten_most_interested_documents.php
@@ -4,6 +4,7 @@ defined('_JEXEC') or die; // no direct access allowed
 require_once dirname(__FILE__).DS.'helper.php'; // get helper files
 
 // get 10 most interested document
-$listMostInterestedDocument = ModTenMostInterestedDocumentsHelper::getMostInterestedDocument(10);
+$modTenMostInterestedDocumentsHelper = new ModTenMostInterestedDocumentsHelper();
+$listMostInterestedDocument = $modTenMostInterestedDocumentsHelper->getMostInterestedDocument(10);
 require JModuleHelper::getLayoutPath('mod_ten_most_interested_documents');
 ?>
