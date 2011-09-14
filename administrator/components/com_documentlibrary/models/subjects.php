132ecfd1b4a6d605991f5e3a59a7cd1253cf64cd
Nguyen Thi Thanh Thuy <thanhthuynguyenhd@gmail.com>
2011-09-14 10:59:53 +0700

    Subject manager component

---
+++ administrator/components/com_documentlibrary/models/subjects.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @version		$Id: contact.php 21148 2011-04-14 17:30:08Z ian $
+ * @package		Joomla.Administrator
+ * @subpackage	com_contact
+ * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access
+defined('_JEXEC') or die;
+
+jimport('joomla.application.component.modellist');
+
+/**
+ * Item Model for a Contact.
+ *
+ * @package		Joomla.Administrator
+ * @subpackage	com_contact
+ * @since		1.6
+ */
+class DocumentlibraryModelSubjects extends JModelList
+{
+	/**
+	 * Method to build an SQL query to load the list data.
+	 *
+	 * @return	string	An SQL query
+	 */
+	protected function getListQuery()
+	{
+		// Create a new query object.		
+		$db = JFactory::getDBO();
+		$query = $db->getQuery(true);
+		// Select some fields
+		$query->select('subject_id, name, published, subject_order');
+		// From the document table
+		$query->from('#__document_subjects');
+		return $query;
+	}
+}
