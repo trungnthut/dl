6ba52c4d67158da2fd096d6fccb4d98f1df7e7b5
Nguyen Thi Thanh Thuy <thanhthuynguyenhd@gmail.com>
2011-09-14 11:03:26 +0700

    Subject manager component

---
+++ administrator/components/com_documentlibrary/models/documentlibrary.php
@@ -0,0 +1,27 @@
+<?php
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+// import the Joomla modellist library
+jimport('joomla.application.component.modellist');
+/**
+ * HelloWorldList Model
+ */
+class DocumentlibraryModelDocumentlibrary extends JModelList
+{
+	/**
+	 * Method to build an SQL query to load the list data.
+	 *
+	 * @return	string	An SQL query
+	 */
+	protected function getListQuery()
+	{
+		// Create a new query object.		
+		$db = JFactory::getDBO();
+		$query = $db->getQuery(true);
+		// Select some fields
+		$query->select('document_id, title');
+		// From the document table
+		$query->from('#__documents');
+		return $query;
+	}
+}
